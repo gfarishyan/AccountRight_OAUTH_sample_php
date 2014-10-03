5c2057ea77fe8082b29a8e2b89761befade908c3
Grigor Farishyan <farishyan@gmail.com>
2014-10-03 13:24:04 +0200

    Changed class

--- class.myob_oauth.php
+++ class.myob_oauth.php
@@ -2,7 +2,7 @@
 // *********************************************************************
 //
 //      Class MYOB API OAUTH
-//         
+//
 //           Sample Written by Keran McKenzie
 //           Date: Feb 2013
 //
@@ -13,84 +13,204 @@
 
 class myob_api_oauth {
 
+  private $version = 'v2';
+  private $api_secret = '';
+  private $scope = 'CompanyFile';
+  private $redirect_url = '';
+  private $api_key = '';
+  private $auth_url = 'https://secure.myob.com/oauth2/v1/authorize';
+  private $access_code = '';
+  private $response = '';
+  private $access_token  = '';
+  private $token_type  = '';
+  private $token_expires_in  = '';
+  private $refresh_token  = '';
 
-	// public function to get an access token
-	public function getAccessToken($api_key, $api_secret, $redirect_url, $access_code, $scope) {
-
-		
-		// build up the params
-		$params = array(
-					'client_id'				=>	$api_key,
-					'client_secret'			=>	$api_secret,
-					'scope'					=>	$scope,
-					'code'					=>	$access_code,
-					'redirect_uri'			=>	$redirect_url,
-					'grant_type'			=>	'authorization_code', // authorization_code -> gives you an access token
-		); // end params array */
-
-		$params = http_build_query($params); // will urlencode data
-		return( $this->getToken($params) );
-
-	}
-
-	// public function to refresh an access token
-	public function refreshAccessToken($api_key, $api_secret, $refresh_token) {
-		// use the getAccessToken function
-		$params = array(
-						'client_id'				=>	$api_key,
-						'client_secret'			=>	$api_secret,
-						'refresh_token'			=>	$refresh_token,
-						'grant_type'			=>	'refresh_token', // refresh_token -> refreshes your access token
-			); // end params array */
-
-		$params = http_build_query($params);
-
-		return( $this->getToken($params) );
-	}
-
-	// private function for token calls
-	private function getToken($params) {
-		
-		//echo $params;
-
-		$response = $this->getURL('https://secure.myob.com/oauth2/v1/authorize', $params);
-		
-		$response_json = json_decode( $response );
-
-		// ***********************
-		//
-		//  TODO: ERROR CHECKING
-		//
-		// **********************
-
-		// if no errors update the tokens
-		return($response_json);
-	}
-
-	// private function for CURL
-	private function getURL($url, $params, $headers=null) {
-
-		$session = curl_init($url); 
-		// do we have headers? set them
-		if( isset($headers) ) {
-			curl_setopt($session, CURLOPT_HTTPHEADER, $headers);
-		}
-		// Tell curl to use HTTP POST
-	    curl_setopt ($session, CURLOPT_POST, true); 
-	    // Tell curl that this is the body of the POST
-	    curl_setopt ($session, CURLOPT_POSTFIELDS, $params); 
-	    // setup the authentication
-	    //curl_setopt($session, CURLOPT_USERPWD, $_SESSION['username'] . ":" . $_SESSION['password']);
-	    // Tell curl not to return headers, but do return the response
-	    curl_setopt($session, CURLOPT_HEADER, false); 
-	    curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+  private $username;
+  private $company_username = '';
+  private $company_password  = '';
+  private $uid;
+  private $guid = '';
+  public $error = array();
+  private $is_post = FALSE;
+  private $debug = array();
+  
+  const COMPANY_BASE_URL = 'https://api.myob.com/accountright/';
+  public function __construct($params = array()) {
+    if (!empty($params)) {
+      foreach ($params as $property => $value) {
+        $this->__set($property, $value);
+      }
+    }
+  }
+  public function set_api_key($key) {
+    $this->api_key = $key;
+  }
 
 
+  public function __set($property, $value) {
+    if (property_exists($this, $property)) {
+      if (is_array($this->$property)) {
+        $this->{$property}[] = $value;
+      } else {
+        $this->{$property} = $value;
+      }
+    } else {
+      $this->{$property} = $value;
+    }
+  }
 
-	    $response = curl_exec($session); 
-	  	curl_close($session);
-	  	//var_dump($response);
-	  	return($response);
-	}
+  public function __get($property) {
+    return $this->{$property};
+  }
 
-} // end class
+
+  public function __call($name, $arguments) {
+     if (method_exists($this, $name)) {
+      return $this->{$name}($arguments);
+     } else {
+       $this->__set('error', 'Killer');
+       return $this->__get('error');
+     }
+  }
+
+  public function getAccessToken() {
+    $params = array(
+      'client_id' => $this->__get('api_key'),
+      'client_secret' => $this->__get('api_secret'),
+      'scope' => $this->__get('scope'),
+      'code' => $this->__get('access_code'),
+      'redirect_uri' => $this->__get('redirect_url'),
+      'grant_type' => 'authorization_code',
+    );
+
+    $this->is_post = TRUE;
+    $this->getURL('https://secure.myob.com/oauth2/v1/authorize', $params, null);
+
+    if (empty($this->error)) {
+      $this->__set('access_token' , $this->response['access_token']);
+      $this->__set('token_type', $this->response['token_type']);
+      $this->__set('token_expires_in', $this->response['expires_in']);
+      $this->__set('refresh_token', $this->response['refresh_token']);
+      $this->__set('username', $this->response['user']['username']);
+      $this->__set('uid', $this->response['user']['uid']);
+    }
+    return $this->response;
+  }
+
+  private function contact_employee($args) {
+    $this->build_auth_header();
+
+  }
+
+
+  private function build_auth_header() {
+    $cftoken = '';
+    $u = $this->__get('company_username');
+    $pwd = $this->__get('company_password');
+    if (!empty($u)) {
+      $cftoken = base64_encode(sprintf("%s:%s", $u, $pwd));
+    }
+
+    $header = array(
+      'Authorization: ' . sprintf("%s %s", ucfirst($this->__get('token_type')), $this->__get('access_token')),
+      'x-myobapi-cftoken: ' . $cftoken,
+      'x-myobapi-key: ' . $this->__get('api_key'),
+      'x-myobapi-version: ' . $this->__get('version'),
+      'Accept-Encoding: '
+    );
+
+    return $header;
+  }
+
+  public function refreshAccessToken() {
+    $params = array(
+      'client_id'  =>	$this->__get('api_key'),
+      'client_secret' =>	$this->__get('api_secret'),
+      'refresh_token' =>	$this->__get('refresh_token'),
+      'grant_type' =>	'refresh_token', // refresh_token -> refreshes your access token
+    );
+    $this->is_post = TRUE;
+    $this->getURL('https://secure.myob.com/oauth2/v1/authorize', $params, null);
+    if (empty($this->error)) {
+      $this->__set('access_token', $this->response['access_token']);
+      $this->__set('token_type', $this->response['token_type']);
+      $this->__set('token_expires_in', $this->response['expires_in']);
+      $this->__set('refresh_token', $this->response['refresh_token']);
+    }
+    return $this->response;
+  }
+
+  public function get_companyFiles() {
+    $header = $this->build_auth_header();
+    $this->is_post = FALSE;
+    $this->getURL(self::COMPANY_BASE_URL, array(), $header);
+    if (!empty($this->error)) {
+      return array();
+    }
+    return $this->response;
+  }
+
+
+
+
+  /***
+   * public function to get curren user info
+   * @params - $guid
+   */
+  public function contact_personal($args) {
+    $header = $this->build_auth_header();
+    $this->is_post = FALSE;
+    
+    //$this->getURL(self::COMPANY_BASE_URL . $args['guid'] . '/CurrentUser', array(), $header); //?$filter=UID eq guid' . " e9250d9a-f17c-48b0-a666-f7ab12428515"
+    $this->getURL('https://api.myob.com/accountright/395cbf41-b746-4622-a5e7-b8fbcc2790b2/Contact/Personal/c2edc3af-d492-4526-83e6-b74a4a80fec7', array(), $header);
+    return $this->response;
+  }
+
+// private function for CURL
+  private function getURL($url, $params = array(), $headers=null) {
+    $this->error = array();
+    $ch = curl_init($url);
+    if( !empty($headers) ) {
+      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    }
+    $q = '';
+    if ($this->is_post) {
+      curl_setopt ($ch, CURLOPT_POST, true);
+      if (is_array($params)) {
+        $q = http_build_query($params);
+      } else {
+        $q = $params;
+      }
+      curl_setopt ($ch, CURLOPT_POSTFIELDS, $q);
+    }
+
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    $r = curl_exec($ch);
+
+    $info = curl_getinfo($ch);
+    $this->debug[] = array(
+        'headers' => $headers,
+        'curl' => $info,
+        'params' => $q
+      );
+    $error = curl_error($ch);
+    if ($error) {
+      $this->error['curl'][curl_errno($ch)] = $error;
+    } elseif ($info['http_code'] !== 200) {
+      $this->error['server'][] = $info['http_code'];
+    }
+    curl_close($ch);
+
+    $r = json_decode($r, TRUE);
+    if (isset($r['error'])) {
+      $this->error['api'][] = $r['error'];
+    }
+
+    $this->response = $r;
+  }
+
+}
